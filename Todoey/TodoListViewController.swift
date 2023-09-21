349ab73f435bc54910c7a7a281524cbbb1aae00c
Volodymyr Kryvytskyi <VolodymyrKryvytskyi@my.uopeople.edu>
2023-09-21 22:00:45 +0900

    Add taskMessages to the tableView

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,8 +10,25 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
+    let itemArray = ["Take a rest", "Hug a kitten", "Do some really big meowws"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let taskMessage = itemArray[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = taskMessage
+        
+        return cell
     }
 }
